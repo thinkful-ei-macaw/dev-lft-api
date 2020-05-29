61d40758ec37b120b3a06f1612df4570187e4687
Malcolm Kiano <46947496+malcolmkiano@users.noreply.github.com>
2020-05-29 15:41:48 -0400

    fix: update join in requests service to reflect true request id (#23)
    * fix: update join in requests service to reflect true request id
    
    * fix: remove console output
    
    * fix: add status pending check to requests service

--- src/requests/requests-service.js
+++ src/requests/requests-service.js
@@ -8,7 +8,7 @@ class RequestsService extends Service {
   getRequests(db, project_id) {
     return super
       .getItemsWhere(db, { 'requests.project_id': project_id, status: 'pending' })
-      .select('requests.*', 'users.*', 'vacancies.title')
+      .select('requests.*', 'users.first_name', 'users.last_name', 'vacancies.title')
       .join('users', { 'users.id': 'requests.user_id' })
       .join('vacancies', { 'vacancies.id': 'requests.vacancy_id' });
   }
