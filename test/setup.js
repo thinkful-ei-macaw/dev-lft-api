d59b97325a955024fe18e8da0d34e9a751f4e124
Sara R Mills <SaraRMills@gmail.com>
2020-06-02 11:51:31 -0400

    fix: removed env vars from test setup file

--- test/setup.js
+++ test/setup.js
@@ -1,15 +1,5 @@
-require('dotenv').config()
-
 const { expect } = require('chai');
 const supertest = require('supertest');
-
-process.env.TZ = 'UTC'
-process.env.NODE_ENV = 'test'
-process.env.JWT_SECRET = 'test-jwt-secret'
-process.env.JWT_EXPIRY = '3m'
-
-process.env.TEST_DATABASE_URL =
-process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/dev-lft',
 
 global.expect = expect;
 global.supertest = supertest;
