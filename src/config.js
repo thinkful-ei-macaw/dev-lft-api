3383f0efb9e7c08690b86a735fb1cae63b77c62a
Muhiddin Kurbonov <muhiddin.m.kurbonov@gmail.com>
2020-06-05 07:48:36 -0700

    removed unused var in config

--- src/config.js
+++ src/config.js
@@ -4,8 +4,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   DATABASE_URL:
     process.env.DATABASE_URL || 'postgresql://postgres@localhost/dev-lft',
-  TEST_DATABASE_URL:
-    process.env.TEST_DATABASE_URL ||
-    'postgresql://postgres@localhost/dev-lft-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 };
