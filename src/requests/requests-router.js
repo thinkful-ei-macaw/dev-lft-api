7954f1027356f012d2d699fb4fdc4159d2da486a
Malcolm Kiano <malcolmkiano@gmail.com>
2020-06-10 02:00:14 -0400

    refactor: update requests-router syntax to async/await

--- src/requests/requests-router.js
+++ src/requests/requests-router.js
@@ -1,25 +1,27 @@
 const express = require('express');
-const { requireAuth } = require('../middleware/jwt-auth');
-const { requireOwner } = require('../middleware/user-role-verification');
-const ProjectsService = require('../projects/projects-service');
 const VacanciesService = require('../vacancies/vacancies-service');
 const RequestsService = require('./requests-service');
+const { requireAuth } = require('../middleware/jwt-auth');
+const { requireOwner } = require('../middleware/user-role-verification');
 
-/**
- * Router to handle all requests to /api/requests
- */
+
 const requestsRouter = express.Router();
-requestsRouter.use(express.json());
+const bodyParser = express.json();
+
+requestsRouter.use(requireAuth);
 
 // POST `/requests` creates a new request
-requestsRouter.post('/:vacancy_id', requireAuth, (req, res, next) => {
-  const db = req.app.get('db');
-  const { vacancy_id } = req.params;
-  const user_id = req.user.id;
+requestsRouter
+  .route('/:vacancy_id')
+  .post(async (req, res, next) => {
+    const db = req.app.get('db');
+    const { vacancy_id } = req.params;
+    const user_id = req.user.id;
 
-  // check if request by user already exists for given vacancy
-  RequestsService.getItemWhere(db, { user_id, vacancy_id })
-    .then(request => {
+    try {
+      // check if request by user already exists for given vacancy
+      const request = await RequestsService.getItemWhere(db, { user_id, vacancy_id });
+
       // if one exists, user already requested to join the team
       if (request)
         return res.status(400).json({
@@ -34,83 +36,88 @@ requestsRouter.post('/:vacancy_id', requireAuth, (req, res, next) => {
       };
 
       // store the request data
-      RequestsService.insertItem(db, newRequest)
-        .then(request => {
-          // send 'em back the request
-          return res.status(201).json(request);
-        })
-        .catch(next);
-    })
-    .catch(next);
-});
+      const dbRequest = await RequestsService.insertItem(db, newRequest);
+
+      // send 'em back the request
+      return res.status(201).json(dbRequest);
+
+    } catch (error) {
+      next(error);
+    }
+  });
 
 // PATCH `/requests/:request_id` marks a request as approved or denied
-requestsRouter.patch('/:id', requireAuth, requireOwner, (req, res, next) => {
-  const db = req.app.get('db');
-  const request_id = req.params.id;
-  const { status } = req.body;
+requestsRouter
+  .route('/:id')
+  .patch(bodyParser, requireOwner, async (req, res, next) => {
+    const db = req.app.get('db');
+    const request_id = req.params.id;
+    const { status } = req.body;
 
-  // check that a status was provided
-  if (!status)
-    return res.status(400).json({
-      error: 'Request body must contain status'
-    });
+    try {
 
-  const updatedRequest = { status: status.toLowerCase() };
+      // check that a status was provided
+      if (!status)
+        return res.status(400).json({
+          error: 'Request body must contain status'
+        });
 
-  // check the validity of the status provided
-  if (
-    updatedRequest.status !== 'approved' &&
-    updatedRequest.status !== 'denied'
-  ) {
-    return res.status(400).send({
-      error: `Status must be either 'approved' or 'denied'`
-    });
-  }
+      const updatedRequest = { status: status.toLowerCase() };
 
-  // update the request
-  RequestsService.updateItem(db, request_id, updatedRequest)
-    .then(request => {
+      // check the validity of the status provided
+      if (
+        updatedRequest.status !== 'approved' &&
+        updatedRequest.status !== 'denied'
+      ) {
+        return res.status(400).send({
+          error: `Status must be either 'approved' or 'denied'`
+        });
+      }
+
+      // update the request
+      const request = await RequestsService.updateItem(db, request_id, updatedRequest)
       if (!request) return;
+
       // if the status was approved
       if (status === 'approved') {
+
         // put the user into the vacancy
         const { user_id, vacancy_id } = request;
         const updatedVacancy = { user_id };
-        VacanciesService.updateItem(db, vacancy_id, updatedVacancy).catch(next);
+        await VacanciesService.updateItem(db, vacancy_id, updatedVacancy);
 
         // deny all other requests for the same vacancy
         const deniedRequest = { status: 'denied' };
-        RequestsService.updateItemsWhere(db, { vacancy_id }, deniedRequest)
-          .whereNot({ id: request.id })
-          .catch(next);
+        await RequestsService.updateItemsWhere(db, { vacancy_id }, deniedRequest).whereNot({ id: request.id });
       }
 
       // send 'em back a thing
       return res.status(204).end();
-    })
-    .catch(next);
-});
+
+    } catch (error) {
+      next(error);
+    }
+  });
 
 // GET `/requests/:project_id` gets all requests for a specific project
-requestsRouter.get(
-  '/:project_id',
-  requireAuth,
-  requireOwner,
-  (req, res, next) => {
+requestsRouter
+  .route('/:project_id')
+  .get(requireOwner, async (req, res, next) => {
     const db = req.app.get('db');
     const { project_id } = req.params;
 
-    // send 'em a list
-    RequestsService.getRequests(db, project_id)
-      .then(requests => {
-        if (res.headersSent) return;
-        return res
-          .status(200)
-          .json(requests.map(RequestsService.serializeRequest));
-      })
-      .catch(next);
-  }
-);
+    try {
+      // send 'em a list
+      const requests = await RequestsService.getRequests(db, project_id)
+      if (res.headersSent) return; // just in case requireOwner had an issue with this
+
+      return res
+        .status(200)
+        .json(requests.map(RequestsService.serializeRequest));
+
+    } catch (error) {
+      next(error);
+    }
+  });
 
 module.exports = requestsRouter;
