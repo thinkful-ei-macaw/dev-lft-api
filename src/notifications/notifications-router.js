cf36d99f7610cfa2dff997c48aa1dbdc658e3eb7
Sara R Mills <SaraRMills@gmail.com>
2020-06-11 14:48:11 -0400

    refactor: PATCH notifications now goes to /notifications and no longer requires a body, updating all notifications for the given user to seen: true

--- src/notifications/notifications-router.js
+++ src/notifications/notifications-router.js
@@ -7,56 +7,32 @@ const { requireAuth } = require('../middleware/jwt-auth');
 
 notificationsRouter.use(requireAuth);
 
-notificationsRouter.route('/').get(async (req, res, next) => {
-  const db = req.app.get('db');
-  const recipient_id = req.user.id;
-
-  try {
-    let notifications = await NotificationsService.getNotifications(
-      db,
-      recipient_id
-    );
-    notifications = notifications.map(
-      NotificationsService.serializeNotifications
-    );
-    res.status(200).json(notifications);
-  } catch (error) {
-    next(error);
-  }
-});
-
 notificationsRouter
-  .route('/:notification_id')
-  .patch(bodyParser, async (req, res, next) => {
+  .route('/')
+  .get(async (req, res, next) => {
     const db = req.app.get('db');
+    const recipient_id = req.user.id;
 
     try {
-      const { seen } = req.body;
-      const { notification_id } = req.params;
-      const newNotification = { seen };
-
-      if (seen === undefined)
-        return res.status(400).json({
-          error: `Request body must contain 'seen'`
-        });
-
-      if (seen !== true && seen !== false)
-        return res.status(400).json({
-          error: `Seen must be 'true' or 'false'`
-        });
-
-      const notification = await NotificationsService.getItemById(
+      let notifications = await NotificationsService.getNotifications(
         db,
-        notification_id
+        recipient_id
       );
-      if (!notification)
-        return res.status(404).json({ error: 'Notification does not exist' });
+      notifications = notifications.map(
+        NotificationsService.serializeNotifications
+      );
+      res.status(200).json(notifications);
+    } catch (error) {
+      next(error);
+    }
+  })
 
-      await NotificationsService.updateItem(
-        db,
-        notification_id,
-        newNotification
-      );
+  .patch(bodyParser, async (req, res, next) => {
+    const db = req.app.get('db');
+    const user_id = { recipient_id: req.user.id };
+    const data = { seen: true };
+    try {
+      await NotificationsService.updateItemsWhere(db, user_id, data);
       res.status(204).end();
     } catch (error) {
       next(error);
